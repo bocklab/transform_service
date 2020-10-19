4d976705da434e218a20d2a08cdfc9634c50871b
Eric Perlman <perlman@gmail.com>
2020-10-19 11:02:21 -0400

    Update datasources to use tsinfo.

--- app/config.py
+++ app/config.py
@@ -32,43 +32,71 @@ DATASOURCES = {
     'flywire_v1' : {
         'description' : 'Mapping from FlyWire (v14.1) to FAFBv14',
         'type' : 'zarr-nested',
-        'path' : '/data/fields/flywire_v1/field.zarr',
         'scales' : [2, 3, 4, 5, 6, 7],
         'voxel_size' : [4,4,40],
         'services' : ['transform'],
         'dtype' : 'float32',
-        'width' : 2
+        'width' : 2,
+        'tsinfo' : {
+            'driver' : 'zarr',
+            'kvstore': {
+                'driver': 'file',
+                'path': '/data/fields/flywire_v1/field.zarr'
+            },
+            'key_encoding': '/'
+        }  
     },
     'flywire_v1_inverse' : {
         'description' : 'Mapping from FAFBv14 to FlyWire (v14.1)',
         'type' : 'zarr-nested',
-        'path' : '/data/fields/v0_inverse/field.zarr',
         'scales' : [4],
         'voxel_size' : [4,4,40],
         'services' : ['transform'],
         'dtype' : 'float32',
-        'width' : 2
+        'width' : 2,
+        'tsinfo' : {
+            'driver' : 'zarr',
+            'kvstore': {
+                'driver': 'file',
+                'path' : '/data/fields/v0_inverse/field.zarr',
+            },
+            'key_encoding': '/'
+        }  
     },
     'fanc_v4_to_v3' : {
         'description' : 'Mapping from FANCv4 to FANCv3',
         'type' : 'zarr-nested',
-        'path' : '/data/fields/fanc/field.zarr',
         'scales' : [2],
         'voxel_size' : [4.3,4.3,45],
         'services' : ['transform'],
         'dtype' : 'float32',
-        'width' : 2
+        'width' : 2,
+        'tsinfo' : {
+            'driver' : 'zarr',
+            'kvstore': {
+                'driver': 'file',
+                'path' : '/data/fields/fanc/field.zarr',
+            },
+            'key_encoding': '/'
+        }  
     },
     'fafb-ffn1-20200412' : {
         'description' : 'fafb-ffn1-20200412 segmentation',
-        'url' : 'precomputed://file:///data/fields/fafb-ffn1-20200412/segmentation',
         'type' : 'neuroglancer_precomputed',
         'scales' : [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
         'voxel_size' : [4,4,40],
         'services' : ['query'],
         'dtype' : 'uint64',
         'width' : 1,
-        'ts_info' : {}
+        'tsinfo' : {
+            'driver' : 'neuroglancer_precomputed',
+            'kvstore': {
+                'driver': 'file',
+                'bucket': 'fafb-ffn1-20200412',
+            },
+            'path': '/data/fields/fafb-ffn1-20200412/segmentation/segmentation',
+        }  
+        
     },
     'fafb-ffn1-20200412-gcs' : {
         'description' : 'fafb-ffn1-20200412 segmentation (access via GCS)',
