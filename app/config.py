2cfc0664a3ba3e862db8f8bd4a4bb4e98cb73d55
Eric Perlman <perlman@gmail.com>
2021-05-19 15:50:58 -0700

    Update FANC downsample.

--- app/config.py
+++ app/config.py
@@ -154,9 +154,10 @@ DATASOURCES = {
     'fanc_v4' : {
         'description' : 'Zetta.ai segmentation of FANCv4 ',
         'type' : 'neuroglancer_precomputed',
+        # These mip levels are those in the segmentation (@ 17.2nm) and NOT the raw data (@4.3nm)
         'scales' : [0, 1, 2, 3, 4, 5],
         'voxel_size' : [17.2,17.2,45],
-        'downsample_factor' : [[1,1,1], [2,2,1], [4,4,1], [8,8,1], [16,16,1], [32,32,1]],
+        'downsample_factor' : [[4,4,1], [8,8,1], [16,16,1], [32,32,1], [64,64,1], [128,128,1]],
         'services' : ['query'],
         'dtype' : 'uint64',
         'width' : 1,
